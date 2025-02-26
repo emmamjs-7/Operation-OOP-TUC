8d843c9239dcaa34a5474dee3a895b06b116e3dd
emmamjs-7 <emmaamjs@gmail.com>
2025-02-26 10:05:20 +0100

    Added plant.cs

--- OperationOOP.Core/Models/Bonsai.cs
+++ OperationOOP.Core/Models/Bonsai.cs
@@ -1,14 +1,8 @@
 namespace OperationOOP.Core.Models;
-public class Bonsai
+public class Bonsai :Plant
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Species { get; set; }
-    public int AgeYears { get; set; }
-    public DateTime LastWatered { get; set; }
-    public DateTime LastPruned { get; set; }
     public BonsaiStyle Style { get; set; }
-    public CareLevel CareLevel { get; set; }
+   
 }
 
 public enum BonsaiStyle
---
+++ OperationOOP.Core/Models/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationOOP.Core.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int AgeYears { get; set; }
+        public DateTime LastWatered { get; set; }
+        public DateTime LastPruned { get; set; }
+        public CareLevel CareLevel { get; set; }
+    }
+}
